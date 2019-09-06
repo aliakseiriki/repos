a3d98990c4e25247082aa1abd63f314b64676ea6
tratsiakou <aliakseiriki@mail.ru>
2019-09-06 22:56:51 +0300

    intermediate

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
 const browsersync = require('browser-sync');
 const fs = require("fs");
-
+​
 const path = {
   distr: {
     html: 'distr/',
@@ -25,17 +25,19 @@ const path = {
     html: 'src/**/[^_]*.html',
     style: 'src/main.scss',
     img: 'src/img/**/*.*',
-    fonts: 'src/fonts/**/*.*'
+    fonts: 'src/fonts/**/*.*',
+    js: 'src/js/**/*.js'
   },
   watch: {
     html: 'src/**/*.html',
     style: 'src/**/*.scss',
     img: 'src/img/**/*.*',
-    fonts: 'src/fonts/**/*.*'
+    fonts: 'src/fonts/**/*.*',
+    js: 'src/js/**/*.js'
   },
   clean: './distr'
 };
-
+​
 const serverConfig = {
   server: {
     baseDir: "./distr"
@@ -46,8 +48,8 @@ const serverConfig = {
   logPrefix: "IT-academy",
   notify: false
 };
-
-
+​
+​
 const cleanDist = (cb) => {
   if(fs.existsSync('./distr')) {
     return src(path.clean)
@@ -55,7 +57,7 @@ const cleanDist = (cb) => {
   }
   cb();
 };
-
+​
 const httpBuild = () => src(path.src.html)
   .pipe(include({
     prefix: '@@',
@@ -63,7 +65,7 @@ const httpBuild = () => src(path.src.html)
   }))
   .pipe(dest(path.distr.html))
   .pipe(browsersync.stream());
-
+​
 const stylesBuild = () => src(path.src.style)
   .pipe(sourcemaps.init())
   .pipe(plumber())
@@ -72,11 +74,12 @@ const stylesBuild = () => src(path.src.style)
   .pipe(sourcemaps.write('./maps/'))
   .pipe(dest(path.distr.css))
   .pipe(browsersync.stream());
-
+​
 const fontsBuild = () => src(path.src.fonts).pipe(dest(path.distr.fonts)).pipe(browsersync.stream());
-
+​
 const imgsBuild = () => src(path.src.img).pipe(dest(path.distr.img)).pipe(browsersync.stream());
-
+const jsBuild = () => src(path.src.js).pipe(dest(path.distr.js)).pipe(browsersync.stream());
+​
 const server = () => {
   browsersync.init(serverConfig);
   
@@ -84,10 +87,11 @@ const server = () => {
   watch(path.watch.style, stylesBuild);
   watch(path.src.img, imgsBuild);
   watch(path.src.fonts, fontsBuild);
+  watch(path.src.js, jsBuild);
   
 };
-
-const build = series(cleanDist, parallel(httpBuild, stylesBuild, fontsBuild, imgsBuild));
+​
+const build = series(cleanDist, parallel(httpBuild, stylesBuild, fontsBuild, imgsBuild, jsBuild));
 exports.start = series(build, server);
 exports.clean = series(cleanDist);
 exports.build = series(build);
