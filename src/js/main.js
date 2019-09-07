d968dc017adebbddb970a01869b404644081aee6
DESKTOP-T0NEQRR\User <aliakseiriki@mail.ru>
2019-09-07 13:52:18 +0300

    da

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,25 @@
-alert('hello world');
+var arr=[];
+function arr_enter() {
+   
+    do {
+        var value=prompt('введите число');
+        arr.push(+value);
+    }
+    while(value!=='')
+}
+
+function arr_sort() {
+
+    for (i=0; i < arr.length; i++) {
+        for (j=i+1; j < arr.length; j++){
+            if (arr[i] > arr[j]) {
+                var a;
+                a=arr[i];
+                arr[i]=arr[j];
+                arr[j]=a;
+            }
+        }
+    }
+
+alert(arr);
+}
